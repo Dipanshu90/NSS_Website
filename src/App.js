5f47e491545f54c7fc38ed578bfad95a775f389f
r-richa <richarani2025@gmail.com>
2023-10-18 20:08:13 +0530

    sample ing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import "./styles.css";
 import ImageCarousel from './components/ImageCarousel';
+import logo1 from './components/images/logo.png';
+import img1 from './components/images/image1.jpg';
+import img2 from './components/images/image2.jpg';
 
 function App() {
   return (
     <div className="homepage">
       <div className="top-header">
-        <img src="logo1.png" alt="Logo 1" className="logo" />
+        <img src={logo1} alt="Logo 1" className="logo" />
         <h1>Your Heading</h1>
         <img src="logo2.png" alt="Logo 2" className="logo" />
       </div>
@@ -30,12 +33,26 @@ function App() {
 
       <div className="two-paragraphs">
         <div className="paragraph">
-          <img src="#" alt="pic 1"/>
-          <p>Paragraph 1 text</p>
+          <img src={img1} alt="pic 1" height={300}/>
+          <p>lorem100
+            lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+            lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+            lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+            lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+
+            
+          </p>
         </div>
         <div className="paragraph">
-          <img src="#" alt="pic 2" />
-          <p>Paragraph 2 text</p>
+        <img src='#' alt="pic 1" height={300}/>
+          <p>lorem100
+            lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+            lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+            lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+            lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+
+            
+          </p>
         </div>
       </div>
 
@@ -43,7 +60,7 @@ function App() {
         {/* Code for recent activities with rolling photos */}
       </div>
 
-      <footer>
+      <footer className='footer'>
         <h2>Contact Details</h2>
         {/* Add your contact details here */}
       </footer>
--- src/components/ImageCarousel.js
+++ src/components/ImageCarousel.js
@@ -2,18 +2,21 @@
 import React from 'react';
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // Import the carousel styles
 import { Carousel } from 'react-responsive-carousel';
+import img1 from './images/image1.jpg';
+import img2 from './images/image2.jpg';
+import img3 from './images/image3.jpg';
 
 const ImageCarousel = () => {
   return (
     <Carousel autoPlay infiniteLoop showThumbs={false}>
       <div>
-        <img src="image1.jpg" alt="Image 1" />
+        <img src={img1} alt="Image 1" height={300} width={200} />
       </div>
       <div>
-        <img src="image2.jpg" alt="Image 2" />
+        <img src={img2} alt="Image 2" height={300} />
       </div>
       <div>
-        <img src="image3.jpg" alt="Image 3" />
+        <img src={img3} alt="Image 3" height={300} />
       </div>
     </Carousel>
   );
